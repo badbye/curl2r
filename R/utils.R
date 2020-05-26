3cd189cd8438ce394356e70bd02ee49b6ad43398
yalei <yaleidu93@gmail.com>
2020-05-26 11:28:41 +0800

    Update utils.R
    para$data: 用list表示

--- R/utils.R
+++ R/utils.R
@@ -10,15 +10,15 @@ post_template <- function(para){
   cmd = "POST('%s', body=%s, add_headers(%s), set_cookies(%s))"
   sprintf(cmd,
           encodeString(para$url),
-          format_vec(para$data),
+          format_vec(para$data, is_list=T),
           format_vec(unlist(para$header)),
           format_vec(unlist(para$cookie)))
 }
 
-format_vec <- function(vec){
+format_vec <- function(vec, is_list=FALSE){
   vec = sapply(seq_along(vec),
                function(i) paste(encodeString(names(vec[i]), quote = "'"), '=', encodeString(vec[i], quote="'")))
   vec = paste0(vec, collapse = ', \n    ')
-  paste0('c(', vec, ')')
+  ifelse(is_list, paste0('list(', vec, ')'), paste0('c(', vec, ')'))
 }
 
